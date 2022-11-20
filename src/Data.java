58ea14eae26da53fbf4172e4ea2e7366a8d796b9
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-20 18:21:03 +0200

    change data to extend Thread

---
+++ src/Data.java
@@ -0,0 +1,16 @@
+public class Data extends Thread
+{
+    private int x = 0;
+    private int y = 0;
+    public Data (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getDiff(){
+        return (Math.abs(x - y));
+    }
+    public synchronized void update(int dx, int dy){
+        x = x + dx;
+        y = y + dy;
+    }
+}
