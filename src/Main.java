28d1969cfe33c717d7c816a0a68a8067d6b8676d
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-11-22 07:22:16 +0200

    add comments methods

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,20 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-
+        // Creating an object of type data and initializing it
         Data data = new Data(0,0);
+
+        // Creating an arraylist of threads
         ArrayList<Thread> threads = new ArrayList<Thread>();
+
+        // Initialization of the required threads
         for (int i = 0 ; i < 4 ; i ++)
         {
             threads.add(new GeneratorThread(data, i));
             threads.add(new CalculatorThread(data, i));
         }
 
-
+        // Start of the required threads
         for (int i = 0 ; i < 8 ; i ++)
         {
             threads.get(i).start();
